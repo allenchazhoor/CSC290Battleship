a6baa23728430c1be9c09f2ffb537f230d1dba0b
George Lewis <georgejeffreylewis@gmail.com>
2019-10-28 16:06:01 -0400

    refactor images, refactor pygame init

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-from gui.app import App
+import pygame, crayons
 
 if __name__ == '__main__':
+
+    pygame.font.init()
+
+    pygame.init()
+
+    print(crayons.green('Initialized PyGame'))
+
+    from gui.app import App
 
     app = App()
 
