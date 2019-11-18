ac9741a47b85a9736b2323d725fd7da7d397ea79
George Lewis <georgejeffreylewis@gmail.com>
2019-11-18 18:13:11 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def install(package):
     else:
         pip._internal.main(['install', package])
 
-if (sys.version_info >= 3 and sys.version_info.minor >= 4):
+if (sys.version_info.major >= 3 and sys.version_info.minor >= 4):
 
     print(f'Python version is too old! Your version is {sys.version.split()[0]} and a version newer than 3.4.0 is required')
 
